9e2e3af994bd46388b66b4a6ff5b5a1ef7e1282d
Egor Grekhnev <egorgrekhnev@mail.ru>
2018-09-30 01:35:46 +0300

    with js

--- serv.js
+++ serv.js
@@ -1,8 +1,12 @@
 var express = require('express')
-
+var path = require('path');
 var app = express();
 
+
 app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
