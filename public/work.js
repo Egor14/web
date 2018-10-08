d9fdcb91eea3987bd295dea22fb72fd11be15284
Egor Grekhnev <egorgrekhnev@mail.ru>
2018-10-08 16:27:37 +0300

    server price

--- public/work.js
+++ public/work.js
@@ -1,17 +1,40 @@
-  var btn = document.getElementById("btnRaise");
+  /*var btn = document.getElementById("btnRaise");
   btn.onclick = function raise(){
     updateTime();
-    updatePrice(5);
-  }
+    updatePrice(5);*/
 
-   var btn2 = document.getElementById("btnYourRaise");
+    /*var xhr = new XMLHttpRequest();
+    var body = document.getElementById("price").innerHTML;
+    xhr.open("POST", '/1', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = ...;
+    xhr.send(body);*/
+    /*document.addEventListener("DOMContentLoaded", function(event) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/1", true);
+
+		xhr.onload = function() {
+			var req = document.getElementById("price").innerHTML;
+			console.log('Egor');
+		};
+
+		xhr.onerror = function() {
+			//alert.log("error")
+		};
+
+		xhr.send(req);
+});*/
+  //}
+
+   /*var btn2 = document.getElementById("btnYourRaise");
    btn2.onclick = function raise(){
    	if (Number(document.getElementById("inputPassword6").value) == 0) return;
     updateTime();
     updatePrice(Number(document.getElementById("inputPassword6").value));
-  }
+  }*/
 
-baseTime = "00:15";
+baseTime = "00:40";
 basePrice = Number(document.getElementById("price").innerHTML);
 document.getElementById("time").innerHTML = baseTime;
 //document.getElementById("price").innerHTML = basePrice;
@@ -42,7 +65,7 @@ function updateTime(){
 	document.getElementById("time").innerHTML = baseTime;
 }
 
-function updatePrice(dollars){
+/*function updatePrice(dollars){
 	basePrice += dollars;
 	document.getElementById("price").innerHTML = basePrice;
-}
+}*/
--- serv.js
+++ serv.js
@@ -1,7 +1,16 @@
-var express = require('express')
+var express = require('express');
+var Pusher = require('pusher');
 var path = require('path');
 var bodyParser = require("body-parser");
 var msgpack = require("msgpack-lite");
+
+var pusher = new Pusher({
+  appId: '616886',
+  key: '861cada503bdd961e815',
+  secret: 'a542ddc62ab5800362df',
+  cluster: 'eu',
+  encrypted: true
+});
 
 var app = express();
 
@@ -15,20 +24,53 @@ app.use(express.static(path.join(__dirname, 'public')));
 var lot_1 = {
   name: "YEEZY BOOST 350 V2 'ZEBRA'",
   url: 'https://raw.githubusercontent.com/Egor14/web/master/yeezy_zebra.png',
-  price : 200
+  price : 200,
+  lotID: 1,
+  time: '00:50'
 };
 
 var lot_2 = {
   name: "NIKE AIR JORDAN",
   url: 'https://raw.githubusercontent.com/Egor14/web/master/jordan.png',
-  price : 100
+  price : 100,
+  lotID: 2,
+  time: '00:50'
 };
 
 var lot_3 = {
   name: "NIKE AIR MAX",
   url: 'https://raw.githubusercontent.com/Egor14/web/master/air.png',
-  price : 70
+  price : 70,
+  lotID: 3,
+  time: '00:50'
 };
+
+function pack(msgpack, id){
+if (id == '1') {
+    var lot_1Json = JSON.stringify(lot_1);
+    var lot_1messPack = msgpack.encode(lot_1Json);
+    return lot_1messPack;
+  } else if (id == '2') {
+    var lot_2Json = JSON.stringify(lot_2);
+    var lot_2messPack = msgpack.encode(lot_2Json);
+    return lot_2messPack;
+  } else if (id == '3') {
+    var lot_3Json = JSON.stringify(lot_3);
+    var lot_3messPack = msgpack.encode(lot_3Json);
+    return lot_3messPack;
+  }  
+
+  /*var lot_1Json = JSON.stringify(lot_1);
+  var lot_2Json = JSON.stringify(lot_2);
+  var lot_3Json = JSON.stringify(lot_3);
+
+  var lot_1messPack = msgpack.encode(lot_1Json);
+  var lot_2messPack = msgpack.encode(lot_2Json);
+  var lot_3messPack = msgpack.encode(lot_3Json);
+
+  return lot_1messPack;*/
+}
+
 
 var lot_1Json = JSON.stringify(lot_1);
 var lot_2Json = JSON.stringify(lot_2);
@@ -40,6 +82,9 @@ var lot_3messPack = msgpack.encode(lot_3Json);
 
 
 app.get('/', function(req, res) {
+  /*pusher.trigger('my-channel', 'my-event', {
+  "message": "hello world"
+  });*/
 	res.sendFile(__dirname + '/index.html');
 });
 
@@ -63,6 +108,22 @@ app.post('/sign', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400);
   console.log(req.body);
   res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/update/:id', urlencodedParser, function (req, res) {
+  if (req.params.id == '1') {
+    lot_1.price += Number(req.body.Price);
+    lot_1messPack = pack(msgpack, req.params.id);
+    res.redirect('/1');
+  } else if (req.params.id == '2') {
+    lot_2.price += Number(req.body.Price);
+    lot_2messPack = pack(msgpack, req.params.id);
+    res.redirect('/2');
+  } else if (req.params.id == '3') {
+    lot_3.price += Number(req.body.Price);
+    lot_3messPack = pack(msgpack, req.params.id);
+    res.redirect('/3');
+  }  
 })
 
 app.listen(3000);
